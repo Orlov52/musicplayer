a80b91efe67258d0a3bc66a6644cceea1985297d
Orlov52 <sanyaorlov52@mail.ru>
2024-04-09 19:37:36 +0300

    feat: add musicplayer

---
+++ orlov/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module org.example.orlov {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
+
+    opens org.example.orlov to javafx.fxml;
+    exports org.example.orlov;
+    exports org.example.demo;
+    opens org.example.demo to javafx.fxml;
+}
